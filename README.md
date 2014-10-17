426cb0b373b3844ba6ba8d6525ae4904459347e9
Christian Zenker <christian.zenker@aoe.com>
2014-10-17 14:37:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+php-raml-parser
+===============
+
+Coverting a [RAML definition](http://raml.org/spec.html) into accessible PHP Objects.
---
+++ tests/AbstractTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Xopn\PhpRamlParserTests;
+
+/**
+ * Base Class for Testing
+ */
+abstract class AbstractTest extends \PHPUnit_Framework_TestCase {
+
+}
